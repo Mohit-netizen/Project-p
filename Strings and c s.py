26cd5758059e843e888ef73eca5d636c68c39463
Mohit44357 <mohitk44357@gmail.com>
2024-10-03 22:07:03 +0530

    Initial commit

---
+++ Strings and c s.py
@@ -0,0 +1,92 @@
+str="this is a string.\nwe are creating it in python"
+print(str)
+
+concatenation-
+str1="apna"
+str2="college"
+Final_str=str1+str2
+print(Final_str)
+
+lenth of str
+str1="apna"
+len1=len(str1)
+print (len1)
+
+Final_str = str1 +"    "+ str2
+print(Final_str)
+print(len(Final_str))
+
+indexing
+char manupulation is not allowed in py (can not store data via assigning)
+str="apna_college"
+ch=str[0]
+print(str[0])
+print(ch)
+
+slicing
+str="APNA COLLEGE"
+print(str[1:4])
+
+str="i am studing python from apna college"
+print (str.endswith("ege"))
+
+str1="apna"
+str2="college"
+val=str1+str2
+print(val)
+print (len(val))
+
+str="apna college"
+ch=str [0]
+print(ch)
+val1=True
+val2=False
+print ("operator" ,val1 or val2)
+
+str='apna college'
+str=str[:]
+
+
+print(str)
+print(str.find("o"))
+
+
+
+age = 17
+
+can_vote= age >= 18
+if can_vote:
+    print("can vote")
+else:
+    print("can not vote")
+
+age = 19
+can_vote = age >= 18  # This evaluates to True since age is 19
+
+print(can_vote)  # Output: True
+
+age1 = 24
+if(True):
+
+elif:
+    print("not")
+    print("vote")
+num=int(input("enter a number"))
+if(num%2==0):
+    print("even")
+else:
+    print("odd")
+
+find largest among 3
+find largest among 3
+find largest among 3
+a=int(input("enter the number1"))
+b=int(input("enter the number2"))
+c=int(input("enter the number3"))
+if (a>=b and a>=c):
+    print("num1 is largest",a)
+elif(b>=c):
+    print("2nd is largest",b)
+else:
+    print("3rd is largest",c)
+
---
+++ va and d ty.py
@@ -0,0 +1,34 @@
+# develped by Guidovan Rossum
+print ("hello world")
+
+val1= True
+val2= True
+sum =a+b
+print (sum)
+
+print(a/b)
+print("and operator",val1 and val2)
+
+name=int(input("enter you name"))
+print("welcome",name)
+print(type(name),name)
+
+val1=int(input("enter 1st number"))
+# val2=int(input("enter 2nd number"))
+print(val1 * val1)
+
+val1=float(input("enter 1st number"))
+val2=float(input("enter 2nd number"))
+print((val1 + val2)/ 2)
+
+
+
+
+# Programming Convenience: By defining programming languages like Python avoid potential errors in certain calculations and algorithms where this expression might arise.
+
+val1=int(input("enter 1st number"))
+val2=int(input("enter 2nd number"))
+val=(val1>=val2)
+print(bool(val))
+
+print(val1>=val2)
