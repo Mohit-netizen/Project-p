c18b023a80491f82c914bdde7b2b93aa1eea4016
Mohit44357 <mohitk44357@gmail.com>
2024-10-04 22:37:41 +0530

    <!-- created-->

---
+++ 3 lis & tup.py
@@ -0,0 +1,6 @@
+student = ["karan", 9.5 , 17 , "delhi"]
+
+student [0] = "arjun"
+print (student[0])
+print(student)
+
---
+++ 5loops.py
@@ -0,0 +1,68 @@
+# count=-1
+# while count <=5:
+#     print("hello")
+#     count+=5
+
+# count=1
+# while count <=100:
+#     print (count)
+#     count+=1
+
+# count=-100
+# while count>=1:
+#     print(count)
+#     count-=1
+
+# table=int(input("enter the number"))
+# i=10
+# while i<=10:
+#     print(table*i)
+#     i+=1
+
+# wall=int(input("enter the number"))
+# i=1
+# while wall <=10:
+#     print(wall,"wall")
+#     wall+=2
+
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# i=0
+# while i<= len(nums):
+#    print(nums)
+#    i+=1
+
+# wall=int(input("enter the number"))
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# i=0
+# while i< len(nums):
+#     print(nums.index(wall))
+#     i+=1
+
+# i=int(input("enter the number"))
+# i=1
+# while i<=10:
+#     if(i%2==0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# veg=("pat","brij","lady finger")
+# for val in veg:
+#     print(val)
+# veg=("1","4","9","25","36","49","64","81","100")
+num=25
+
+i=0
+for el in veg:
+    if(el==num):
+        print("ok")
+i+=1
+
+
+
+
+
+
+
+
